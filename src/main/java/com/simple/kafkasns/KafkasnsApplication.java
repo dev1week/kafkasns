5f62be689da8446f3d7b7a9abbfdbd429a674be0
dev1week <rondo2860@gmail.com>
2023-11-07 06:14:56 +0900

    Initial commit

---
+++ src/main/java/com/simple/kafkasns/KafkasnsApplication.java
@@ -0,0 +1,13 @@
+package com.simple.kafkasns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkasnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkasnsApplication.class, args);
+    }
+
+}
